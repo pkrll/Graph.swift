986dca938da9d0de3065491df090492a967f050f
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-03-31 14:12:06 +0200

    Added weight to an edge

--- Sources/GraphCore/Edge.swift
+++ Sources/GraphCore/Edge.swift
@@ -9,10 +9,16 @@ public class Edge {
 
 	public let incoming: Node
 	public let outgoing: Node
+	public let weight: Int
 
-	init(from incoming: Node, to outgoing: Node) {
+	convenience init(from incoming: Node, to outgoing: Node) {
+		self.init(from: incoming, to: outgoing, withWeight: 0)
+	}
+
+	init(from incoming: Node, to outgoing: Node, withWeight weight: Int) {
 		self.incoming = incoming
 		self.outgoing = outgoing
+		self.weight = weight
 	}
 
 }
