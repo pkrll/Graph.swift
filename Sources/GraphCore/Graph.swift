c76437e5f427998bff71afc021729fb2d0f9092a
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-03-31 14:07:14 +0200

    Implemented addNode and subscript methods of Graph

--- Sources/GraphCore/Graph.swift
+++ Sources/GraphCore/Graph.swift
@@ -7,4 +7,32 @@
 
 public class Graph {
 
+	public private(set) var nodes: [Node] = []
+
+	public var size: Int {
+		return self.nodes.count
+	}
+
+	public subscript(_ nodeLabel: Int) -> Node? {
+		for node in self.nodes where node.label == nodeLabel {
+			return node
+		}
+
+		return nil
+	}
+
+	public func addNode(withLabel label: Int) {
+		let node = Node(withLabel: label)
+		self.nodes.insert(node, at: 0)
+	}
+
+	public func addNode(_ node: Node) {
+		let size = self.nodes.count
+		let node = Node(withLabel: size)
+		self.nodes.insert(node, at: size)
+	}
+
+	public func addEdge(from label: Int, to toLabel: Int, withWeight weight: Int) {
+
+	}
 }
